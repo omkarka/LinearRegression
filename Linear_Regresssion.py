f7e1f7b4c37a448eb74f27784240a3cee4e26192
omkarka <omkarkambli96@gmail.com>
2021-04-06 18:51:26 +0530

    Add files via upload

---
+++ Linear_Regresssion.py
@@ -0,0 +1,59 @@
+
+### Load Libraries
+"""
+ 
+import pandas as pd
+ 
+"""### Load Data"""
+ 
+path = r"https://drive.google.com/uc?export=download&amp;id=1xxDtrZKfuWQfl-6KA9XEd_eatitNPnkB" 
+df = pd.read_csv(path)
+ 
+df.head()
+ 
+"""### Split Data"""
+ 
+X = df.drop('price', axis=1)
+y = df['price']
+ 
+print('Shape of X = ', X.shape)
+print('Shape of y = ', y.shape)
+ 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=51)
+ 
+print('Shape of X_train = ', X_train.shape)
+print('Shape of y_train = ', y_train.shape)
+print('Shape of X_test = ', X_test.shape)
+print('Shape of y_test = ', y_test.shape)
+ 
+"""### Feature Scaling"""
+ 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+X_train = sc.transform(X_train)
+X_test = sc.transform(X_test)
+ 
+"""## Linear Regression - ML Model Training"""
+ 
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+ 
+lr.fit(X_train, y_train)
+ 
+lr.coef_
+ 
+lr.intercept_
+ 
+"""## Predict the value of Home and Test"""
+ 
+X_test[0, :]
+ 
+lr.predict([X_test[0, :]])
+ 
+lr.predict(X_test)
+ 
+y_test
+ 
+lr.score(X_test, y_test)
